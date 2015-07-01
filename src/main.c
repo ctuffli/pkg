a51354add67a0e79a4bb757ad7863aa080fb59c2
Manuel Wiesinger <mdw@freebsd.org>
2015-07-01 14:14:45 +0200

    refactored REPO_PKG to PKG_BINARY

--- src/main.c
+++ src/main.c
@@ -331,8 +331,8 @@ show_repository_info(void)
 			break;
 		}
 		switch(pkg_repo_type(repo)) {
-		case REPO_PKG:
-			type = "PKG";
+		case REPO_BINARY:
+			type = "BINARY";
 		break;
 		case REPO_DEBIAN:
 			type = "DEBIAN";
